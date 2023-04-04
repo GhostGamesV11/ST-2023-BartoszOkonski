8975249450089715e021e162a1900826d066a29f
GhostGamesV11 <47499005+GhostGamesV11@users.noreply.github.com>
2023-04-04 19:06:25 +0200

    Update main.py

--- zad3/main.py
+++ zad3/main.py
@@ -175,6 +175,42 @@ perceptron.fit(X, d)
 
 print(perceptron.predict(X))
 
+# ZADANIE 4
+# Napisz algorytm propagacji wstecznej do reprezentacji x1 XOR x2.
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def sigmoid_derivative(x):
+    return x * (1 - x)
+
+X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+y = np.array([[0], [1], [1], [0]])
+
+np.random.seed(1)
+synapse_0 = 2 * np.random.random((2, 4)) - 1
+synapse_1 = 2 * np.random.random((4, 1)) - 1
+
+for j in range(60000):
+
+    layer_0 = X
+    layer_1 = sigmoid(np.dot(layer_0, synapse_0))
+    layer_2 = sigmoid(np.dot(layer_1, synapse_1))
+
+    layer_2_error = y - layer_2
+
+    if (j % 10000) == 0:
+        print("Error: " + str(np.mean(np.abs(layer_2_error))))
+
+    layer_2_delta = layer_2_error * sigmoid_derivative(layer_2)
+    layer_1_error = layer_2_delta.dot(synapse_1.T)
+    layer_1_delta = layer_1_error * sigmoid_derivative(layer_1)
+
+    synapse_1 += layer_1.T.dot(layer_2_delta)
+    synapse_0 += layer_0.T.dot(layer_1_delta)
+
+print("Wynik po nauczeniu:")
+print(layer_2)
 
 
 
